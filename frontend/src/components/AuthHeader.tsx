ee26f983fcc004b6338f25946afc0c935d840309
ZyBacon <22520316@gm.uit.edu.vn>
2025-11-13 17:22:14 +0700

    feat: add login, register pages

---
+++ frontend/src/components/AuthHeader.tsx
@@ -0,0 +1,28 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+
+export default function AuthHeader({ title }: { title: string }) {
+  return (
+    <View style={styles.logoContainer}>
+      {/* <Image style={styles.logo} source={require("../assets/react-logo.png")} /> */}
+      <Text style={styles.logoText}>{title}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  logoContainer: {
+    alignItems: "center",
+  },
+
+  logo: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+  },
+
+  logoText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+});
---
+++ frontend/src/const/Color.ts
@@ -0,0 +1,7 @@
+export const Colors = {
+  white: "#FFFFFF",
+  gray: "#BEBEBE",
+  silver: "#F8F8F8",
+  black: "#000000",
+  midnightBlue: "#191970",
+};
