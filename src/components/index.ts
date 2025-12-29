b7a33a499dcfcd87cdb390cdab8acc71e2ddb11d
Lê Đức Anh Duy <115405058+duyle1062@users.noreply.github.com>
2025-12-29 17:54:27 +0700

    [BE] feat: done rename collection function (#34)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { OCRImagePreview } from "./OCRImagePreview";
 export { OCRStatsBar } from "./OCRStatsBar";
 export { OCRTextEditor } from "./OCRTextEditor";
 export { OCRActionBar } from "./OCRActionBar";
+export { default as RenameCollectionModal } from "./RenameCollectionModal";
